76d3dadee659e358b18dc283021c731611b46756
externalip <129664957+externalip@users.noreply.github.com>
2023-12-15 10:36:27 +0800

    Update transmission estimation parameters

--- dehazing/dehazing.py
+++ dehazing/dehazing.py
@@ -40,7 +40,7 @@ class DehazingCPU(object):
         return A
 
     def TransmissionEstimate(self, im, A, sz):
-        omega = 0.95
+        omega = 0.90
         im3 = np.empty(im.shape, im.dtype)
 
         for ind in range(0, 3):
@@ -63,11 +63,27 @@ class DehazingCPU(object):
 
     def image_processing(self, frame):
         I = frame.astype('float64') / 255
+
+        start = time.time()
         dark = self.DarkChannel(I, 15)
+        print(f"Dark: {time.time() - start}")
+
+        start = time.time()
         A = self.EstimateA(I, dark)
-        te = self.TransmissionEstimate(I, A, 15)
+        print(f"Atmos: {time.time() - start}")
+
+        start = time.time()
+        te = self.TransmissionEstimate(I, A, 3)
+        print(f"Tmap: {time.time() - start}")
+
+        start = time.time()
         t = self.GaussianTransmissionRefine(te)
+        print(f"Gauss: {time.time() - start}")
+
+        start = time.time()
         J = self.Recover(I, t, A, 0.1)
+        print(f"J: {time.time() - start}")
+
         return J
 
 
