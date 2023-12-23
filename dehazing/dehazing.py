745573bcbfac047c1ce52056f20ea17f9e7ace41
externalip <129664957+externalip@users.noreply.github.com>
2023-12-23 20:37:06 +0800

    Optimize dehazing code for efficiency

--- dehazing/dehazing.py
+++ dehazing/dehazing.py
@@ -25,26 +25,17 @@ class DehazingCPU(object):
         h, w, _ = img.shape
         length = h * w
         num = max(int(length * 0.0001), 1)
-        # convert to a row vector
-        darkChannVec = np.reshape(darkChannel, length)
-        index = darkChannVec.argsort()[length - num:]
-        rowIdx = index // w
-        colIdx = index % w
-        coords = np.stack((rowIdx, colIdx), axis=1)
 
-        sumA = np.zeros((1, 3))
-        for coord in coords:
-            row, col = coord
-            sumA += img[row, col, :]
-        A = sumA / num
+        darkChannVec = darkChannel.reshape(length)
+        index = np.argpartition(darkChannVec, -num)[-num:]
+        coords = np.column_stack(np.unravel_index(index, (h, w)))
+
+        A = np.mean(img[coords[:, 0], coords[:, 1], :], axis=0, keepdims=True)
         return A
 
     def TransmissionEstimate(self, im, A, sz):
         omega = 0.90
-        im3 = np.empty(im.shape, im.dtype)
-
-        for ind in range(0, 3):
-            im3[:, :, ind] = im[:, :, ind] / A[0, ind]
+        im3 = im / A[0, :]
 
         transmission = 1 - omega * self.DarkChannel(im3, sz)
         return transmission
@@ -53,13 +44,11 @@ class DehazingCPU(object):
         return gaussian_filter(et, sigma=sigma)
 
     def Recover(self, im, t, A, tx=0.1):
-        res = np.empty(im.shape, im.dtype)
-        t = cv2.max(t, tx)
-
-        for ind in range(0, 3):
-            res[:, :, ind] = (im[:, :, ind] - A[0, ind]) / t + A[0, ind]
-
+        t = np.maximum(t, tx)
+        t_broadcasted = np.expand_dims(t, axis=-1)  
+        res = (im - A) / t_broadcasted + A
         return res
+
 
     def image_processing(self, frame):
         I = frame.astype('float64') / 255
